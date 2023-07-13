4ff03642935c9e8de464891da466dbf86df3ea3a
jonma0107 <jduplantier44@gmail.com>
2023-07-12 22:28:43 -0500

    [ADD] crud for Employees model

--- EmployeeApp/views.py
+++ EmployeeApp/views.py
@@ -33,6 +33,35 @@ def departmentApi(request, id=0):
         department.delete()
         return JsonResponse("Delete Successfully", safe=False)
 
+"""
+employees
+"""
+
+@csrf_exempt
+def employeeApi(request, id=0):
+    if request.method == 'GET':
+        employees = Employees.objects.all()
+        employees_serializer=EmployeeSerializer(employees, many=True)
+        return JsonResponse(employees_serializer.data, safe=False)
+    elif request.method == 'POST':
+        employee_data = JSONParser().parse(request)
+        employees_serializer=EmployeeSerializer(data=employee_data)
+        if employees_serializer.is_valid():
+            employees_serializer.save()
+            return JsonResponse("Added Successfully", safe=False)
+    elif request.method == 'PUT':
+        employee_data = JSONParser().parse(request)
+        employee = Employees.objects.get(EmployeeId=employee_data['EmployeeId'])
+        employees_serializer=EmployeeSerializer(employee, data=employee_data)
+        if employees_serializer.is_valid():
+            employees_serializer.save()
+            return JsonResponse("Update Successfully", safe=False)
+        return JsonResponse("Failed to Update") 
+    elif request.method == 'DELETE':
+        employee = Employees.objects.get(EmployeeId=id)
+        employee.delete()
+        return JsonResponse("Delete Successfully", safe=False)        
+
 
 
 
